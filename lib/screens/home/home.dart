ef1b32c7ecc6232dfbc6ffc33202c70930b9fa2a
ldiaz06 <luisdiaz060@gmail.com>
2022-02-23 09:27:22 -0500

    Inicio de Proyecto. Mucha suerte Luis :)

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:sap_app/constants/colors.dart';
+import 'package:sap_app/widgets/custom_app_bar.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var selected = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackground,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(
+            Icons.arrow_back_ios_outlined,
+            Icons.search_outlined,
+          )
+        ],
+      ),
+    );
+  }
+}
